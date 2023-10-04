1c20efa50a7eb5a58468da86b13ceebda9d3db7f
Aniruddha2308 <79984355+Aniruddha2308@users.noreply.github.com>
2023-10-04 16:09:02 +0530

    Update prgm4.c

--- Task_4/prgm4.c
+++ Task_4/prgm4.c
@@ -2,7 +2,7 @@
 #include <string.h>
 void main() 
 {
-    int flag=0,index,i,j,k,x,len1,len2,len3;
+    int flag=0,i,j,k,len1,len2,len3;
     char a[100],b[100],c[100],d[100];
     
     printf("Enter a str:\n");
@@ -17,8 +17,6 @@ void main()
     len1=strlen(a);
     len2=strlen(b);
     len3=strlen(c);
-    
-    x=len1-len3;
     
     for(i=0;i<=(len1 - len2);i++)
     {
